0578ba9ca148e413b9a098d37a64912b3168dee8
Kayla Alvarado <kayla.alvarado@hotmail.com>
2021-12-11 00:42:09 -0500

    Rudimentary image-picking capability added. User's gallery app opens when they try to add Photo to album

--- app/src/main/java/softmeth/android/AlbumFragment.java
+++ app/src/main/java/softmeth/android/AlbumFragment.java
@@ -1,7 +1,13 @@
 package softmeth.android;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
@@ -9,12 +15,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AlbumFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AlbumFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -22,12 +24,34 @@ public class AlbumFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_album, container, false);
 
+        // For handling the image picker
+        ActivityResultLauncher<Intent> imagePicker = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        String imagePath = result.getData().getData().getPath();
+                        Toast.makeText(view.getContext(), "Image " + imagePath + " successfully loaded", Toast.LENGTH_LONG).show();
+                    }
+                });
+
         // Create onClickListener for opening the selected photo
         Button openButton = view.findViewById(R.id.open_photo_button);
         openButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Navigation.findNavController(view).navigate(R.id.action_albumFragment_to_photoFragment);
+            }
+        });
+
+        // Open user's Gallery and prompt them to add a photo
+        Button addButton = view.findViewById(R.id.add_photo_button);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                imagePicker.launch(intent);
             }
         });
 
